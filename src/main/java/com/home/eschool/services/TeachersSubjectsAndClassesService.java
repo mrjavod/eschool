824506c29d13171384b88a9e75764f3677e3fa16
mrjavod <javod1995@gmail.com>
2022-10-27 15:02:14 +0500

    some fix

--- src/main/java/com/home/eschool/services/TeachersSubjectsAndClassesService.java
+++ src/main/java/com/home/eschool/services/TeachersSubjectsAndClassesService.java
@@ -183,7 +183,7 @@ public class TeachersSubjectsAndClassesService implements CrudInterface<Teachers
                                     t.getClasses().getName())));
         }
 
-        return list;
+        return list.stream().distinct().collect(Collectors.toList());
     }
 
     public List<SubjectsPayload> getTeacherSubjectsByClass(UUID classId) {
